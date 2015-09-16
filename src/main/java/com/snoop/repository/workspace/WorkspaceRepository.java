c3ff8c2f8b8eab17ea2a9523d10b4e3c3a62a249
to-more <to_more@mailoo.org>
2015-09-16 00:37:24 -0300

    add redis repo to cache workspaces

--- src/main/java/com/snoop/repository/workspace/WorkspaceRepository.java
+++ src/main/java/com/snoop/repository/workspace/WorkspaceRepository.java
@@ -8,24 +8,13 @@ import com.snoop.model.workspace.Workspace;
 
 public interface WorkspaceRepository {
 
-	/**
-	   * The User that is saved from this method will be stored in the
-	   * cache and referenced by its' ID.
-	   */
 	  @CachePut("workspaces")
 	  public Workspace save(Workspace workspace);
 
-	  /**
-	   * This method should never actually be executed, since the User will
-	   * always be retrieved from the cached output of saveUser.
-	   */
+
 	  @Cacheable("workspaces")
 	  public Workspace get(Long id);
 
-	  /**
-	   * When this method is called, the cached User will be deleted from
-	   * the cache.
-	   */
 	  @CacheEvict("workspaces")
 	  public void delete(Long id);
 }
